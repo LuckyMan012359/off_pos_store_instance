87d8212a99e37e48bc8faac2c31388f23c33e8f9
LuckyMan012359 <rorifloris@gmail.com>
2025-02-24 20:55:30 +0000

    fix: complete create and update item from main to sub and from sub to main

--- application/controllers/api/v1/ApiItemController.php
+++ application/controllers/api/v1/ApiItemController.php
@@ -382,90 +382,113 @@ class ApiItemController extends REST_Controller
             $item_updated_id = $find_item_id->id;
             $company_info = getCompanyInfoByAPIKey($item_info['api_auth_key']);
             if ($company_info) {
-                $company_id = $company_info->id;
-                $user_id = $company_info->user_id;
-                $unit_type = $item_info['unit_type'];
-                $opening_stock = json_decode(str_replace("'", '"', $item_info['opening_stock']), true);
-                $itemArr = array();
-                $itemArr['code'] = $item_info['code'];
-                $itemArr['name'] = $item_info['name'];
-                $itemArr['photo'] = $item_info['photo'];
-                $itemArr['alternative_name'] = $item_info['alternative_name'];
-                $itemArr['type'] = $item_info['type'];
-                $itemArr['p_type'] = $item_info['type'];
-                $itemArr['loyalty_point'] = $item_info['loyalty_point'];
-                $itemArr['profit_margin'] = $item_info['profit_margin'];
-                if ($item_info['category_name'] != '') {
-                    $itemArr['category_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['category_name'], 'name', 'tbl_item_categories', $user_id, $company_id);
-                } else {
-                    $itemArr['category_id'] = '';
+                $itemErr = [];
+                if ($item_info['name'] == '') {
+                    $error = true;
+                    $itemErr['name'] = 'The Name field is required';
                 }
-                if ($item_info['brand_name'] != '') {
-                    $itemArr['brand_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['brand_name'], 'name', 'tbl_brands', $user_id, $company_id);
-                } else {
-                    $itemArr['brand_id'] = '';
+                if ($item_info['sale_price'] == '') {
+                    $error = true;
+                    $itemErr['sale_price'] = 'The Sale Price field is required';
                 }
-                if ($item_info['supplier_name']) {
-                    $itemArr['supplier_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['supplier_name'], 'name', 'tbl_suppliers', $user_id, $company_id);
-                } else {
-                    $itemArr['supplier_id'] = '';
+                if ($item_info['category_name'] == '') {
+                    $error = true;
+                    $itemErr['category_name'] = 'The Category Name field is required';
                 }
-                $itemArr['alert_quantity'] = $item_info['alert_quantity'];
-                if ($item_info['purchase_unit_name'] != '') {
-                    $itemArr['purchase_unit_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['purchase_unit_name'], 'unit_name', 'tbl_units', $user_id, $company_id);
-                } else {
-                    $itemArr['purchase_unit_id'] = '';
+                if ($item_info['unit_type'] == '') {
+                    $error = true;
+                    $itemErr['unit_type'] = 'The Unit Type field is required';
                 }
-                if ($item_info['sale_unit_name'] != '') {
-                    $itemArr['sale_unit_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['sale_unit_name'], 'unit_name', 'tbl_units', $user_id, $company_id);
-                } else {
-                    $itemArr['sale_unit_id'] = '';
-                }
-                if ($unit_type == 'Single Unit') {
-                    $itemArr['conversion_rate'] = 1;
-                    $itemArr['unit_type'] = 1;
-                } elseif ($unit_type == 'Double Unit') {
-                    $itemArr['conversion_rate'] = $item_info['conversion_rate'];
-                    $itemArr['unit_type'] = 2;
-                }
-                $itemArr['purchase_price'] = $item_info['purchase_price'];
-                $itemArr['whole_sale_price'] = $item_info['whole_sale_price'];
-                $itemArr['warranty'] = $item_info['warranty'];
-                $itemArr['warranty_date'] = $item_info['warranty_type'];
-                $itemArr['guarantee'] = $item_info['guarantee'];
-                $itemArr['guarantee_date'] = $item_info['guarantee_type'];
-                $itemArr['description'] = $item_info['description'];
-                $itemArr['sale_price'] = $item_info['sale_price'];
-                $itemArr['user_id'] = $user_id;
-                $itemArr['company_id'] = $company_id;
-                // Tax
-                $tax_info = json_decode(str_replace("'", '"', $item_info['tax_information']), true);
-                $tax_name = array();
-                foreach ($tax_info as $tax) {
-                    $tax_name[] = $tax['tax_field_name'];
-                }
-                $tax_string = implode(':', $tax_name);
-                if ($tax_string === $company_info->tax_string) {
-                    $itemArr['tax_information'] = json_encode($tax_info);
-                    $itemArr['tax_string'] = $tax_string;
-                } else {
-                    $response = array(
-                        'status' => 404,
-                        'message' => "Tax String doesn't match",
-                    );
-                }
-                $this->Common_model->insertInformation($itemArr, "tbl_items");
-                $this->Common_model->deletingMultipleFormData('item_id', $item_updated_id, 'tbl_set_opening_stocks');
-                $this->saveOpeningStock($opening_stock, $item_info['type'], $item_info['conversion_rate'], $item_updated_id, $itemArr['user_id'], $company_id);
-                if ($item_updated_id) {
-                    $response = array(
-                        'status' => 200,
-                        'message' => 'Data inserted successful.'
-                    );
+                if ($error == false) {
+                    $company_id = $company_info->id;
+                    $unit_type = $item_info['unit_type'];
+                    $opening_stock = json_decode(str_replace("'", '"', $item_info['opening_stock']), true);
+                    $itemArr = array();
+                    $itemArr['code'] = $item_info['code'];
+                    $itemArr['name'] = $item_info['name'];
+                    $itemArr['photo'] = $item_info['photo'];
+                    $itemArr['alternative_name'] = $item_info['alternative_name'];
+                    $itemArr['type'] = $item_info['type'];
+                    $itemArr['p_type'] = $item_info['type'];
+                    $itemArr['loyalty_point'] = $item_info['loyalty_point'];
+                    $itemArr['profit_margin'] = $item_info['profit_margin'];
+                    if ($item_info['category_name'] != '') {
+                        $itemArr['category_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['category_name'], 'name', 'tbl_item_categories', 0, $company_id);
+                    } else {
+                        $itemArr['category_id'] = '';
+                    }
+                    if ($item_info['brand_name'] != '') {
+                        $itemArr['brand_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['brand_name'], 'name', 'tbl_brands', 0, $company_id);
+                    } else {
+                        $itemArr['brand_id'] = '';
+                    }
+                    if ($item_info['supplier_name']) {
+                        $itemArr['supplier_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['supplier_name'], 'name', 'tbl_suppliers', 0, $company_id);
+                    } else {
+                        $itemArr['supplier_id'] = '';
+                    }
+                    $itemArr['alert_quantity'] = $item_info['alert_quantity'];
+                    if ($item_info['purchase_unit_name'] != '') {
+                        $itemArr['purchase_unit_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['purchase_unit_name'], 'unit_name', 'tbl_units', 0, $company_id);
+                    } else {
+                        $itemArr['purchase_unit_id'] = '';
+                    }
+                    if ($item_info['sale_unit_name'] != '') {
+                        $itemArr['sale_unit_id'] = $this->Common_model->fieldNameCheckingByFieldNameForAPI($item_info['sale_unit_name'], 'unit_name', 'tbl_units', 0, $company_id);
+                    } else {
+                        $itemArr['sale_unit_id'] = '';
+                    }
+                    if ($unit_type == 'Single Unit') {
+                        $itemArr['conversion_rate'] = 1;
+                        $itemArr['unit_type'] = 1;
+                    } elseif ($unit_type == 'Double Unit') {
+                        $itemArr['conversion_rate'] = $item_info['conversion_rate'];
+                        $itemArr['unit_type'] = 2;
+                    }
+                    $itemArr['purchase_price'] = $item_info['purchase_price'];
+                    $itemArr['whole_sale_price'] = $item_info['whole_sale_price'];
+                    $itemArr['warranty'] = $item_info['warranty'];
+                    $itemArr['warranty_date'] = $item_info['warranty_type'];
+                    $itemArr['guarantee'] = $item_info['guarantee'];
+                    $itemArr['guarantee_date'] = $item_info['guarantee_type'];
+                    $itemArr['description'] = $item_info['description'];
+                    $itemArr['sale_price'] = $item_info['sale_price'];
+                    $itemArr['added_date'] = date('Y-m-d H:i:s');
+                    $itemArr['user_id'] = 0;
+                    $itemArr['company_id'] = $company_id;
+                    $tax_info = json_decode(str_replace("'", '"', $item_info['tax_information']), true);
+                    $tax_name = array();
+                    foreach ($tax_info as $tax) {
+                        $tax_name[] = $tax['tax_field_name'];
+                    }
+                    $tax_string = implode(':', $tax_name) . ":";
+                    if ($tax_string === $company_info->tax_string) {
+                        $itemArr['tax_information'] = json_encode($tax_info);
+                        $itemArr['tax_string'] = $tax_string;
+                        $insertedId = $this->Common_model->insertInformation($itemArr, "tbl_items");
+                        $this->saveOpeningStock($opening_stock, $item_info['type'], $item_info['conversion_rate'], $insertedId, $itemArr['user_id'], $company_id);
+                        if ($insertedId) {
+                            $response = array(
+                                'status' => 200,
+                                'message' => 'Data inserted successful.',
+                                'opening_stock' => $opening_stock,
+                            );
+                        } else {
+                            $response = array(
+                                'status' => 400,
+                                'message' => 'Insertion failded something wrong',
+                            );
+                        }
+                    } else {
+                        $response = array(
+                            'status' => 404,
+                            'message' => "Tax String doesn't match",
+                        );
+                    }
                 } else {
                     $response = array(
                         'status' => 400,
-                        'message' => 'Insertion failded something wrong',
+                        'message' => $itemErr,
                     );
                 }
             } else {
